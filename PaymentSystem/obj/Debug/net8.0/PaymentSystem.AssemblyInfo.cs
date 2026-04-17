95c057b33b73971cbfe8790c01707e8f521e5433
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-04-17 05:08:28 +0200

    Updated GlobalTrust UI and improvements

--- PaymentSystem/obj/Debug/net8.0/PaymentSystem.AssemblyInfo.cs
+++ PaymentSystem/obj/Debug/net8.0/PaymentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5cca180302ba9ba33f43dd0551e830d3baf6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc1954e4d3ba7903217b216ab6b0b44d582d860")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
